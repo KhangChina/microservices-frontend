0e7bcac2a68d4176ef521b5c889265729f0a21e7
Khang China <nguyenkhang1400@gmail.com>
2024-01-05 16:42:51 +0700

    update

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   hmr: false,
-  apiUrl: 'http://localhost:4000'
+  apiUrl: 'http://192.168.100.99:3002',
+  productID: 'e2eab0ea-fb88-4bfe-96c8-adc4cc86bcaa'
 };
